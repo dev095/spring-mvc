b87ce6b8c3e5805e2261c35957b854721c6a2e48
Sergey Smurov <s7878896@gmail.com>
2021-02-18 16:06:53 +0300

    add stream api in getCars

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,27 +1,35 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class CarService {
-
-    private static int CAR_ID;
 
     public List<Car> cars;
 
     {
         cars = new ArrayList<>();
+
         cars.add(new Car("VOLVO", "S70", "Red"));
         cars.add(new Car("BMW", "X5", "Black"));
         cars.add(new Car("MAZDA", "7", "Green"));
         cars.add(new Car("LADA", "Kalina", "Rusty"));
         cars.add(new Car("AUDI", "8", "Yellow"));
+
     }
 
     public List<Car> getCars(Integer count) {
-        int id = count != null && count < cars.size() ? count : cars.size();
-        return cars.subList(0, id);
+        if ((count == null) || (count >= 5) || (count < 0)) {
+            return cars;
+        } else {
+            return cars.stream().limit(count).collect(Collectors.toList());
+        }
+//        int id = count != null && count < cars.size() ? count : cars.size();
+//        return cars.subList(0, id);
     }
 }
